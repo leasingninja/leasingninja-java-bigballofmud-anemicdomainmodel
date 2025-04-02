5577598de439d547498fc7f9173c3121409c273b
Henning Schwentner <hs@wps.de>
2025-04-02 13:01:54 +0200

    Test calculation with interest of 0

--- src/test/java/io/leasingninja/domain/ContractTest.java
+++ src/test/java/io/leasingninja/domain/ContractTest.java
@@ -22,5 +22,24 @@ public class ContractTest {
 		// then
 		assertThat(contract.getInstallment()).isEqualTo(897.80);
 	}
-   
+
+	@Test
+	void givenAFilledOutContractWith0Interest_whenCalculate_thenInstallmentIsX() {
+		// given
+		Contract contract = new Contract();
+        contract.setNumber("4711");
+		contract.setLessee("John Buyer");
+		contract.setCar("Volkswagen ID.3");
+		contract.setPrice(40_000);
+        //contract.setCurrency("EUR"));
+
+		// when
+    	ContractService.calculateInstallmentFor(contract, 48, 0);
+
+		// then
+		assertThat(contract.getInstallment()).isEqualTo(833.33);
+	}
+
+    // no givenACalculatedContract_whenSign_thenContractIsSigned() since we just call setSignDate()
+
 }
